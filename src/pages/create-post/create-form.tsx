0f67926619293e4080a2fe02994ec6d7484a9aa7
Daniel Kubowicz <daniel.kubowicz10@gmail.com>
2025-10-30 09:09:29 +0100

    Enhance CreateForm component with form validation and Firestore integration

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,23 +1,31 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
+import { addDoc, collection  } from "firebase/firestore";
+import { db } from "../../config/firebase";
+interface IFormInput {
+  title: string;
+  description: string;
+}
 
 export const CreateForm = () => {
   const schema = yup.object().shape({
     title: yup.string().required("You must add a title."),
     description: yup.string().required("You must add a description"),
   });
-  const {register,handleSubmit} = useForm({
+  const {register,handleSubmit, formState:{errors}} = useForm({
     resolver :yupResolver(schema)
 })
 
-const onCreatePost = (data:any) =>{
+const onCreatePost = (data:IFormInput) =>{
     console.log(data);
 }
   return (
     <form onSubmit={handleSubmit(onCreatePost)}>
       <input placeholder="Title..." {...register("title")}/>
+      <p>{errors.title?.message}</p>
       <input placeholder="Description..." {...register("description")}/>
+      <p>{errors.description?.message}</p>
       <input type="submit" />
     </form>
   );
