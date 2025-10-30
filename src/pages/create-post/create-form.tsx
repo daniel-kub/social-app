b242951db5ac0eae53e868cb878dbf4dbf84179b
Daniel <daniel.kubowicz10@gmail.com>
2025-10-30 08:42:25 +0100

    Second commit

---
+++ src/pages/create-post/create-form.tsx
@@ -0,0 +1,8 @@
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+export const CreateForm = () => {
+    return <form>
+
+    </form>;
+}
